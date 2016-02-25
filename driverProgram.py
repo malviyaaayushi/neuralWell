b2111131cff44b91ac8f26213bf512e4bddc688e
Aayushi Malviya <malviyaaayushi@gmail.com>
2016-02-25 14:29:21 +0530

    Code running succesfully

--- driverProgram.py
+++ driverProgram.py
@@ -1,7 +1,7 @@
-import network3
-from network3 import Network
-from network3 import ConvPoolLayer, FullyConnectedLayer, SoftmaxLayer
-training_data, validation_data, test_data = network3.load_data_shared()
+import network
+from network import Network
+from network import ConvPoolLayer, FullyConnectedLayer, SoftmaxLayer
+training_data, validation_data, test_data = network.load_data_shared()
 mini_batch_size = 10
 net = Network([FullyConnectedLayer(n_in=784, n_out=100), SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size) 
 net.SGD(training_data, 60, mini_batch_size, 0.1, validation_data, test_data)
