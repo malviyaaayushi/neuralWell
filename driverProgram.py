56091011d1ffa399d8b6fbdbbb0b2e4cc3171c39
Aayushi Malviya <malviyaaayushi@gmail.com>
2016-03-07 17:33:47 +0530

    Code working for simulated data. Need to improve accuracy

--- driverProgram.py
+++ driverProgram.py
@@ -9,8 +9,9 @@ import numpy as np
 import theano
 import theano.tensor as T
 
+
 #training_data, validation_data, test_data = network.load_data_shared()
 training_data, validation_data, test_data = network.load_data_shared("data/data.pkl.gz")
-mini_batch_size = 1000
+mini_batch_size = 10
 net = Network([FullyConnectedLayer(n_in=784, n_out=100), SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size) 
 net.SGD(training_data, 60, mini_batch_size, 0.1, validation_data, test_data)
